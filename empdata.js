0630904351c833b70952370421df26589a77606a
ehutch09 <ehutch09@users.noreply.github.com>
2017-04-14 20:34:51 -0700

    Update empdata.js

--- empdata.js
+++ empdata.js
@@ -1,124 +1,127 @@
 {
-  "data": [
-    {
-      "name": "Garrett Winters",
-      "designation": "Accountant",
-      "salary": "$170,750",
-      "joining_date": "2011/07/25",
-      "office": "Tokyo",
-      "extension": "8422"
-    },
-    {
-      "name": "Brielle Williamson",
-      "designation": "Integration Specialist",
-      "salary": "$372,000",
-      "joining_date": "2012/12/02",
-      "office": "New York",
-      "extension": "4804"
-    },
-    {
-      "name": "Ashton Cox",
-      "designation": "Junior Technical Author",
-      "salary": "$86,000",
-      "joining_date": "2009/01/12",
-      "office": "San Francisco",
-      "extension": "1562"
-    },
-    {
-      "name": "Airi Satou",
-      "designation": "Accountant",
-      "salary": "$162,700",
-      "joining_date": "2008/11/28",
-      "office": "Tokyo",
-      "extension": "5407"
-    },
-    {
-      "name": "Caesar Vance",
-      "designation": "Pre-Sales Support",
-      "salary": "$106,450",
-      "joining_date": "2011/12/12",
-      "office": "New York",
-      "extension": "8330"
-    },
-    {
-      "name": "Shad Decker",
-      "designation": "Regional Director",
-      "salary": "$183,000",
-      "joining_date": "2008/11/13",
-      "office": "Edinburgh",
-      "extension": "6373"
-    },
-    {
-      "name": "Cedric Kelly",
-      "designation": "Senior Javascript Developer",
-      "salary": "$433,060",
-      "joining_date": "2012/03/29",
-      "office": "Edinburgh",
-      "extension": "6224"
-    },
-    {
-      "name": "Haley Kennedy",
-      "designation": "Senior Marketing Designer",
-      "salary": "$313,500",
-      "joining_date": "2012/12/18",
-      "office": "London",
-      "extension": "3597"
-    },
-    {
-      "name": "Colleen Hurst",
-      "designation": "Javascript Developer",
-      "salary": "$205,500",
-      "joining_date": "2009/09/15",
-      "office": "San Francisco",
-      "extension": "2360"
-    },
-    {
-      "name": "Dai Rios",
-      "designation": "Personnel Lead",
-      "salary": "$217,500",
-      "joining_date": "2012/09/26",
-      "office": "Edinburgh",
-      "extension": "2290"
-    },
-    {
-      "name": "Herrod Chandler",
-      "designation": "Sales Assistant",
-      "salary": "$137,500",
-      "joining_date": "2012/08/06",
-      "office": "San Francisco",
-      "extension": "9608"
-    },
-    {
-      "name": "Rhona Davidson",
-      "designation": "Integration Specialist",
-      "salary": "$327,900",
-      "joining_date": "2010/10/14",
-      "office": "Tokyo",
-      "extension": "6200"
-    },
-    {
-      "name": "Sonya Frost",
-      "designation": "Software Engineer",
-      "salary": "$103,600",
-      "joining_date": "2008/12/13",
-      "office": "Edinburgh",
-      "extension": "1667"
-    },
-    {
-      "name": "Jena Gaines",
-      "designation": "Office Manager",
-      "salary": "$90,560",
-      "joining_date": "2008/12/19",
-      "office": "London",
-      "extension": "3814"
-    },
-    {
-      "name": "Quinn Flynn",
-      "designation": "Support Lead",
-      "salary": "$342,000",
-      "joining_date": "2013/03/03",
-      "office": "Edinburgh",
-      "extension": "9497"
-    }
-  ]
+   "root": {
+      "data": [
+         {
+            "name": "Allen, Keenan",
+            "designation": "WR",
+            "salary": "$170,750",
+            "joining_date": "4/20/2012",
+            "office": "California",
+            "extension": "13"
+         },
+         {
+            "name": "Barksdale, Joe",
+            "designation": "T",
+            "salary": "$372,000",
+            "joining_date": "4/20/2010",
+            "office": "LSU",
+            "extension": "72"
+         },
+         {
+            "name": "Benjamin, Travis",
+            "designation": "WR",
+            "salary": "$86,000",
+            "joining_date": "4/20/2011",
+            "office": "Miami (Fla.)",
+            "extension": "12"
+         },
+         {
+            "name": "Franklin, Orlando",
+            "designation": "G",
+            "salary": "$162,700",
+            "joining_date": "4/20/2010",
+            "office": "Miami (Fla.)",
+            "extension": "74"
+         },
+         {
+            "name": "Gates, Antonio",
+            "designation": "TE",
+            "salary": "$106,450",
+            "joining_date": "4/20/2002",
+            "office": "Kent State",
+            "extension": "85"
+         },
+         {
+            "name": "Gordon, Melvin",
+            "designation": "RB",
+            "salary": "$183,000",
+            "joining_date": "4/20/2015",
+            "office": "Wisconsin",
+            "extension": "28"
+         },
+         {
+            "name": "Henry, Hunter",
+            "designation": "TE",
+            "salary": "$433,060",
+            "joining_date": "4/20/2016",
+            "office": "Arkansas",
+            "extension": "86"
+         },
+         {
+            "name": "Inman, Dontrelle",
+            "designation": "WR",
+            "salary": "$313,500",
+            "joining_date": "4/20/2013",
+            "office": "Virginia",
+            "extension": "15"
+         },
+         {
+            "name": "McCluster, Dexter",
+            "designation": "RB",
+            "salary": "$205,500",
+            "joining_date": "4/20/2010",
+            "office": "Mississippi",
+            "extension": "33"
+         },
+         {
+            "name": "Okung, Russell",
+            "designation": "OT",
+            "salary": "$217,500",
+            "joining_date": "4/20/2009",
+            "office": "Oklahoma State",
+            "extension": "76"
+         },
+         {
+            "name": "Pulley, Spencer",
+            "designation": "C",
+            "salary": "$137,500",
+            "joining_date": "4/20/2015",
+            "office": "Vanderbilt",
+            "extension": "73"
+         },
+         {
+            "name": "Rivers, Philip",
+            "designation": "QB",
+            "salary": "$327,900",
+            "joining_date": "4/20/2003",
+            "office": "North Carolina State",
+            "extension": "17"
+         },
+         {
+            "name": "Slauson, Matt",
+            "designation": "C-G",
+            "salary": "$103,600",
+            "joining_date": "4/20/2008",
+            "office": "Nebraska",
+            "extension": "68"
+         },
+         {
+            "name": "Williams, Tyrell",
+            "designation": "WR",
+            "salary": "$90,560",
+            "joining_date": "4/20/2014",
+            "office": "Western Oregon",
+            "extension": "16"
+         },
+         {
+            "name": "Watt, Derek",
+            "designation": "FB",
+            "salary": "$342,000",
+            "joining_date": "4/20/2016",
+            "office": "Wisconsin",
+            "extension": "34"
+         }
+      ],
+      "_xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance"
+   }
 }
